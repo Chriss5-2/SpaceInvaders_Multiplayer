50196ccc190e3e30983837a0ccd745675d83db1d
Chriss5-2 <chris.luna.j@uni.pe>
2025-05-05 02:06:43 -0500

    Add an proyectil list to set all the proyectils

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,13 +1,17 @@
 package Game;
 
+import java.util.ArrayList;
+
 public class Player{
     private String name;
     private int x, y;
+    private ArrayList<Proyectil> proyectiles = new ArrayList<>(); // Creando lista de proyectiles
 
     public Player(String name,int x, int y){
         this.name = name;
         this.x = x;
         this.y = y;
+        this.proyectiles = new ArrayList<>(); // Creando lista de proyectiles en el constructor
     }
 
     public int posX(){
@@ -24,6 +28,14 @@ public class Player{
     public void position(int x, int y){
         this.x = x;
         this.y = y;
+    }
+
+    public ArrayList<Proyectil> getProyectiles(){
+        return proyectiles;
+    }
+
+    public void disparar(){
+        proyectiles.add(new Proyectil(x, y-1));
     }
 
 }
